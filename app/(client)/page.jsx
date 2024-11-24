bcd081813bc58155ce5d9f0414bd524e6291fd16
khoanguyen-dona <khoa.nguyen.anh8796@gmail.com>
2024-11-24 20:25:31 +0700

    Home page : responsive mobile

--- app/(client)/page.jsx
+++ app/(client)/page.jsx
@@ -7,7 +7,7 @@ import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import React from 'react'
-import Footer from '@/components/Footer';
+
 
 const Home = () => {
   return (
@@ -36,7 +36,7 @@ const Home = () => {
       </div>
 
       {/* Giày carousel */}
-      <div className='text-center text-7xl font-bold mb-5 mt-10' >
+      <div className='text-center text-5xl md:text-6xl  xl:text-7xl font-bold mb-5 mt-10' >
         <h1>Giày</h1>
       </div>
       <div className='mb-10'  >
@@ -44,7 +44,7 @@ const Home = () => {
       </div>
 
       {/* Quần áo carousel */}
-      <div className='text-center text-7xl font-bold mb-5' >
+      <div className='text-center text-5xl md:text-6xl  xl:text-7xl font-bold mb-5' >
         <h1>Quần áo</h1>
       </div>
       <div className='mb-10'>
@@ -52,7 +52,7 @@ const Home = () => {
       </div>
 
       {/* Phụ kiện carousel */}
-      <div className='text-center text-7xl font-bold mb-5' >
+      <div className='text-center text-5xl md:text-6xl  xl:text-7xl font-bold mb-5' >
         <h1>Phụ kiện</h1>
       </div>
       <div>
@@ -62,9 +62,9 @@ const Home = () => {
    
 
       {/* Thông tin về ShoeShop */}
-      <div className=' mt-20 bg-black text-white py-10 px-80 '  >
+      <div className=' mt-20 bg-black text-white py-10 px-4  md:px-10 lg:px-20 xl:px-80  flex flex-col '  >
         <h1 className='text-4xl text-center ' >Thông tin về <strong> DonaStore </strong> </h1>
-        <p className='mt-10 items-center' >Donastore là công ty chuyên cung cấp giày thể thao chất lượng cao, được thành lập với sứ mệnh mang lại sự kết hợp hoàn hảo giữa phong cách và hiệu suất cho mọi vận động viên và tín đồ thể thao. Chúng tôi cung cấp một bộ sưu tập đa dạng, từ giày chạy bộ, giày bóng rổ đến giày tập gym, phù hợp với nhu cầu của từng khách hàng.
+        <p className='mt-10 text-left' >Donastore là công ty chuyên cung cấp giày thể thao chất lượng cao, được thành lập với sứ mệnh mang lại sự kết hợp hoàn hảo giữa phong cách và hiệu suất cho mọi vận động viên và tín đồ thể thao. Chúng tôi cung cấp một bộ sưu tập đa dạng, từ giày chạy bộ, giày bóng rổ đến giày tập gym, phù hợp với nhu cầu của từng khách hàng.
 
         Với đội ngũ thiết kế tài năng, Donastore không ngừng cập nhật xu hướng mới nhất và ứng dụng công nghệ tiên tiến trong sản xuất để tạo ra những đôi giày không chỉ thời trang mà còn bảo vệ sức khỏe cho đôi chân. Mỗi sản phẩm đều được kiểm tra chất lượng nghiêm ngặt trước khi đến tay người tiêu dùng, đảm bảo sự thoải mái và độ bền cao.
 
--- components/ProductCarousel.jsx
+++ components/ProductCarousel.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useRouter } from 'next/navigation';
+import { FormatCurrency } from '@/utils/FormatCurrency';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination ,Scrollbar} from 'swiper/modules';
@@ -14,11 +15,16 @@ const ProductCarousel = ({data}) => {
         <Swiper
             className='w-full h-2/4 items-center justify-center '
             spaceBetween={5}
-            slidesPerView={4}
+            slidesPerView={2}
+            breakpoints={{
+                480: { slidesPerView: 2, spaceBetween: 30 },
+                768: { slidesPerView: 3, spaceBetween: 30 },
+                1024: { slidesPerView: 4, spaceBetween: 30 },
+              }}
             loop={false}
             pagination={{ clickable: true }}
             navigation={true}
-            scrollbar = {true}
+            scrollbar = {false}
             modules={[Navigation, Pagination,Scrollbar]}
         >
             {data.map((d)=>(
@@ -26,8 +32,8 @@ const ProductCarousel = ({data}) => {
                     <a href={`/product-detail/${d.id}`}>
                         <img className='flex object-cover  items-center justify-center' src={d.thumbnail} alt="Slide 1" />
                         <div className='text-center' >
-                            <p>{d.productName}</p>
-                            <p>{d.price} đ</p>
+                            <p className='font-semibold'>{d.productName}</p>
+                            <p>{FormatCurrency(d.price)} đ</p>
                         </div>
                     </a>               
                 </SwiperSlide>
