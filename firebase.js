b30f77cc0b268c85624e798c8154f8582e54f7f8
nguyen_anh_khoa <151300565+khoanguyen-dona@users.noreply.github.com>
2024-12-05 17:19:17 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCY0CmY2_9HiVtYfJbOoVMmqrTzY62fgxI",
+  apiKey: ,
   authDomain: "adidas-shop-d0636.firebaseapp.com",
   projectId: "adidas-shop-d0636",
   storageBucket: "adidas-shop-d0636.appspot.com",
